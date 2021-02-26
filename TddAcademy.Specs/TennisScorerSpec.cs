sven82/TDDAcademyCore

added exercise skeleton

TddAcademy.Specs/TennisScorerSpec.cs
@@ -1,0 +1,7 @@
+﻿namespace TddAcademy.Spec
+{
+    public class TennisScorerSpec
+    {
+        // todo: implement TennisScorer acceptance test first 
+    }
+}

jkulcsar/TDDAcademy

TddAcademy.Specs/TennisScorerSpec.cs
@@ -1,0 +1,7 @@
+﻿namespace TddAcademy.Specs
+{
+    public class TennisScorerSpec
+    {
+        // todo: implement TennisScorer acceptance test first 
+    }
+}